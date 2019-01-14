df7c9b64487c9cdb2a6a819ac79bd633c0b6316e
Hyunjin Kim <firadazer@gmail.com>
2019-01-13 23:52:20 -0500

    Contains a class that has all the global variables for this project - initial commit

---
+++ codes/GlobalVar.py
@@ -0,0 +1,16 @@
+###
+#   File name : GlobalVar.py
+#   Author    : Hyunjin Kim
+#   Date      : Jan 13, 2019
+#   Email     : firadazer@gmail.com
+#   Purpose   : Contains a class that has all the global variables for this project
+#
+#   Instruction
+#               1. import GlobalVar
+#               2. Run the function DBControl.start() - specify the inputs
+#               3. It will help you to connect to the database
+###
+
+
+
+
